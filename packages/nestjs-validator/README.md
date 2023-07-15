e74c5cd5be1afadb499e7abbcd26e539cdca0e7b
Jose Guillermo <jguillermo@outlook.com>
2023-07-15 14:46:19 -0500

    make validator

---
+++ packages/nestjs-validator/README.md
@@ -0,0 +1,11 @@
+# nestjs-validator
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nestjs-validator` to build the library.
+
+## Running unit tests
+
+Run `nx test nestjs-validator` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/nestjs-validator/src/validator/domain-validator.ts
@@ -0,0 +1,29 @@
+import { ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from 'class-validator';
+import { ValidatorInterface } from '@archi-code/common';
+
+@ValidatorConstraint({ name: 'domainValidator', async: false })
+export class DomainValidator implements ValidatorConstraintInterface {
+  private static factoryType(value: any, objClass: any): ValidatorInterface {
+    const vo: ValidatorInterface = new objClass(value);
+    return vo;
+  }
+
+  validate(value: any, args: ValidationArguments): Promise<boolean> | boolean {
+    try {
+      const vo = DomainValidator.factoryType(value, args.constraints[0]);
+      return vo.isValid();
+    } catch (e) {
+      return false;
+    }
+  }
+
+  defaultMessage(args: ValidationArguments): string {
+    try {
+      const vo = DomainValidator.factoryType(args.value, args.constraints[0]);
+      vo.isValid();
+      return `${args.property}: ${vo.validatorMessage()}`;
+    } catch (e: any) {
+      return `${args.property}: ${e.message}`;
+    }
+  }
+}
